f2d57ed90d7949e4ec8c77558aa1ee3631fe3bd2
APB97 <adrian.bieliski.pl@gmail.com>
2019-05-17 09:11:00 +0200

    Added redirect to home page on login and check for user's existance.

--- BoardGamesRentalApplication/BoardGamesRentalApplication/Controllers/LoginController.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication/Controllers/LoginController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 using BoardGamesRentalApplication.Models;
-
 
 namespace BoardGamesRentalApplication.Controllers
 {
@@ -20,11 +17,18 @@ namespace BoardGamesRentalApplication.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [HandleError(ExceptionType = typeof(Exception), View = "Error")]
         public ActionResult Login(User userEntity)
         {
             using (MySqlDbContext db = new MySqlDbContext())
             {
                 var user = db.Users.Where(u => u.Username == userEntity.Username).FirstOrDefault();
+                if (user == null)
+                {
+                    ViewBag.Message = "Taki użytkownik nie istnieje.";
+                    return View();
+                }
+
                 var pass = userEntity.Password;
                 byte[] saltedPassword = Encoding.UTF8.GetBytes(pass).Concat(user.Salt).ToArray();
                 using (var sha = SHA256.Create())
@@ -33,12 +37,16 @@ namespace BoardGamesRentalApplication.Controllers
                     byte[] hashForComparison = Convert.FromBase64String(user.Password);
                     for (int i = 0; i < hashForComparison.Length; i++)
                         if (hash[i] != hashForComparison[i])
-                            throw new UnauthorizedAccessException();
+                        {
+                            ViewBag.Message = "Niepoprawne hasło.";
+                            return View();
+                        }
                     //access granted
-                    Session["logged"] = true;
+                    Session["UserId"] = user.Id;
+                    Session["Username"] = user.Username;
                 }
             }
-            return View();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
