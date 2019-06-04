6fa22bf4852282cdc4abb3f3b016bb24526ec4f8
APB97 <adrian.bieliski.pl@gmail.com>
2019-06-04 20:03:57 +0200

    Updated README.md, added constructor with IUnitOfWork parameter to LoginService.

--- BoardGamesRentalApplication/BoardGamesRentalApplication.BLL/Service/LoginService.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.BLL/Service/LoginService.cs
@@ -10,36 +10,40 @@ namespace BoardGamesRentalApplication.BLL.Service
 {
     public class LoginService : ILoginService
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public LoginService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
         public LoginServiceResponse Login(User user)
         {
             try
             {
-                using (UnitOfWork unit = new UnitOfWork())
+                User matchingUser = unitOfWork.UserRepository.FindBy(u => u.Username == user.Username).FirstOrDefault();
+                if (matchingUser == null)
                 {
-                    var matchingUser = unit.UserRepository.FindBy(u => u.Username == user.Username).FirstOrDefault();
-                    if (matchingUser == null)
+                    return LoginServiceResponse.UserDoesntExist;
+                }
+                else
+                {
+                    string password = user.Password;
+                    byte[] saltedPassword = Encoding.UTF8.GetBytes(password).Concat(matchingUser.Salt).ToArray();
+                    using (SHA256 sha = SHA256.Create())
                     {
-                        return LoginServiceResponse.UserDoesntExist;
-                    }
-                    else
-                    {
-                        string password = user.Password;
-                        byte[] saltedPassword = Encoding.UTF8.GetBytes(password).Concat(matchingUser.Salt).ToArray();
-                        using (SHA256 sha = SHA256.Create())
+                        byte[] hash = sha.ComputeHash(saltedPassword);
+                        byte[] hashForComparison = Convert.FromBase64String(matchingUser.Password);
+                        for (int i = 0; i < hashForComparison.Length; i++)
                         {
-                            byte[] hash = sha.ComputeHash(saltedPassword);
-                            byte[] hashForComparison = Convert.FromBase64String(matchingUser.Password);
-                            for (int i = 0; i < hashForComparison.Length; i++)
+                            if (hash[i] != hashForComparison[i])
                             {
-                                if (hash[i] != hashForComparison[i])
-                                {
-                                    return LoginServiceResponse.IncorrectPassword;
-                                }
+                                return LoginServiceResponse.IncorrectPassword;
                             }
-                            matchingUser.LastLogin = DateTime.Now;
-                            unit.Save();
-                            return LoginServiceResponse.LoginSuccessful;
                         }
+                        matchingUser.LastLogin = DateTime.Now;
+                        unitOfWork.Save();
+                        return LoginServiceResponse.LoginSuccessful;
                     }
                 }
             }
