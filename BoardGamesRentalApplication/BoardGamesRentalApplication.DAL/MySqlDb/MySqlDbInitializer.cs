94356a637570f2f19aeab35877e42cc92989b6fd
APB97 <adrian.bielinski.pl@gmail.com>
2019-09-26 18:17:15 +0200

    Added Administrator user to database in Seed method. Changed from deciding if user is an admin by using User.UserType from the database instead of calculating it from username. Fixed Login/Register links so they do not appear after successful login. And ajdusted Admin's Panel list item classes.

--- BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbInitializer.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbInitializer.cs
@@ -12,7 +12,34 @@ namespace BoardGamesRentalApplication.DAL.MySqlDb
     {
         protected override void Seed(MySqlDbContext context)
         {
-            
+            byte[] implicitPassword;
+            string explicitPassword = "1234321PasswordHere";
+            byte[] salt = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] password = Encoding.UTF8.GetBytes(explicitPassword);
+                byte[] saltedPassword = password.Concat(salt).ToArray();
+                implicitPassword = sha512.ComputeHash(saltedPassword);
+            }
+
+            User adminUser = new User
+            {
+                Email = "bielikba@wp.pl",
+                Username = "ADMINISTRATOR",
+                Salt = salt,
+                Password = Convert.ToBase64String(implicitPassword),
+                FirstName = "Ja",
+                LastName = "Admin",
+                UserType = UserType.Administrator,
+                CreateDate = DateTime.Now,
+                LastLogin = DateTime.MinValue
+            };
+            context.Users.Add(adminUser);
+            context.SaveChanges();
         }
     }
 }
