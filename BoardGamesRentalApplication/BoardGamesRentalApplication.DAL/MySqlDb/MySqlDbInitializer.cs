968e5a729880025c68f7388baabeaba8b80251b9
GrzegorzTarczykowski <ghardghard123@gmail.com>
2019-09-30 21:11:28 +0200

    fix after merge

--- BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbInitializer.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbInitializer.cs
@@ -1,11 +1,3 @@
-using BoardGamesRentalApplication.DAL.Models;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System;
 using System.Data.Entity;
 
 namespace BoardGamesRentalApplication.DAL.MySqlDb
