3fd3caae8860eba3a01b701e2bd48a320f03a86b
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2019-10-11 07:05:42 +0200

    Added AdminPanel with link to Catategory Management. Added Administrator account int database initialization, so AdminPanel is availible to see when using this account.

--- BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbInitializer.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbInitializer.cs
@@ -1,6 +1,10 @@
 using BoardGamesRentalApplication.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BoardGamesRentalApplication.DAL.MySqlDb
 {
@@ -66,6 +70,34 @@ namespace BoardGamesRentalApplication.DAL.MySqlDb
                                                     , BoardGameState = defaultBoardGameStates[2] });
 
             context.BoardGames.AddRange(defaultBoardGames);
+
+            byte[] implicitPassword;
+            string explicitPassword = "1234321PasswordHere";
+            byte[] salt = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] password = Encoding.UTF8.GetBytes(explicitPassword);
+                byte[] saltedPassword = password.Concat(salt).ToArray();
+                implicitPassword = sha512.ComputeHash(saltedPassword);
+            }
+
+            User adminUser = new User
+            {
+                Email = "bielikba@wp.pl",
+                Username = "ADMINISTRATOR",
+                Salt = salt,
+                Password = Convert.ToBase64String(implicitPassword),
+                FirstName = "Ja",
+                LastName = "Admin",
+                UserType = UserType.Administrator,
+                CreateDate = DateTime.Now,
+                LastLogin = DateTime.MinValue
+            };
+            context.Users.Add(adminUser);
         }
     }
 }
---
+++ BoardGamesRentalApplication/BoardGamesRentalApplication/Controllers/AdminPanelController.cs
@@ -0,0 +1,20 @@
+﻿using BoardGamesRentalApplication.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardGamesRentalApplication.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: AdminPanel
+        public ActionResult Index()
+        {
+            if (!UserType.Administrator.Equals(Session["UserType"]))
+                return RedirectToAction("Index", "Home");
+            return View();
+        }
+    }
+}
