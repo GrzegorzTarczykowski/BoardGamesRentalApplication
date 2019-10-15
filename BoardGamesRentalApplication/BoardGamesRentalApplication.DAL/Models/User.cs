594342113b43961de55aeb22d93592a61d618672
Glowix <lo.glowacki.6@gmail.com>
2019-10-15 09:23:47 +0200

    Update

--- BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/Models/User.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/Models/User.cs
@@ -11,7 +11,6 @@ namespace BoardGamesRentalApplication.DAL.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public UserType UserType { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime CreateDate { get; set; }
--- BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbContext.cs
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/MySqlDb/MySqlDbContext.cs
@@ -18,7 +18,6 @@ namespace BoardGamesRentalApplication.DAL.MySqlDb
         public DbSet<BoardGamePublisher> BoardGamePublishers { get; set; }
         public DbSet<BoardGameState> BoardGameStates { get; set; }
         public DbSet<BoardGameCategory> BoardGameCategories { get; set; }
-        public DbSet<BoardGamePublisher> BoardGamePublishers { get; set; }
         public DbSet<BoardGameType> BoardGameTypes { get; set; }
         public DbSet<User> Users { get; set; }
 
@@ -39,7 +38,7 @@ namespace BoardGamesRentalApplication.DAL.MySqlDb
                 .WithOptional();
             modelBuilder.Entity<BoardGamePublisher>()
                 .HasMany<BoardGame>(bgp => bgp.BoardGames)
-                .WithRequired(bg => bg.Publisher)
+                .WithRequired(bg => bg.BoardGamePublisher)
                 .WillCascadeOnDelete(false);//TODO: Decide if should cascade delete or not
         }
     }
