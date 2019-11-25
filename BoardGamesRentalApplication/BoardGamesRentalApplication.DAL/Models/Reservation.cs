48e4f10090528e5d065d87cf66f5b7768d89acd7
Grzegorz <Grzegorz@DESKTOP-NA6BLEO>
2019-11-25 23:32:31 +0100

    add reservation and repair filter

---
+++ BoardGamesRentalApplication/BoardGamesRentalApplication.DAL/Models/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGamesRentalApplication.DAL.Models
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public DateTime RentalStartDate { get; set; }
+        public DateTime RentalEndDate { get; set; }
+        public int Count { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int BoardGameId { get; set; }
+        public BoardGame BoardGame { get; set; }
+        public int ReservationStatusId { get; set; }
+        public ReservationStatus ReservationStatus { get; set; }
+    }
+}
